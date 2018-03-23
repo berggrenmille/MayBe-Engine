e54e4ab730f6e077ab9a41c9d6eaed9c2a3dd9a4
iceklue <iceklue@gmail.com>
2018-03-23 17:58:18 +0100

    V0.0.1
    Project start
    
    added Window class
        A very primitive display abstraction
        Not complete

---
+++ MayBeEngine/MayBeEngine/Display.cpp
@@ -0,0 +1,69 @@
+﻿#include "stdafx.h"
+#include "Display.h"
+
+Window::Window(int width, int height)
+	: m_internalWindow(nullptr), m_windowID(-1), m_width(width), m_height(height), m_mouseFocus(true), m_keyboardFocus(true), m_fullScreen(false), m_minimized(false), m_shown(true)
+{}
+
+//Init window
+bool Window::Initialize()
+{
+	m_internalWindow = std::make_unique<GLFWwindow*>(glfwCreateWindow(m_width, m_height, "Test", nullptr, nullptr));
+	if(!m_internalWindow.get())
+	{
+		glfwTerminate();
+		return false;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(GetInternalWindow());
+
+	return true;
+}
+
+void Window::Destroy()
+{
+	glfwDestroyWindow(GetInternalWindow());
+}
+
+int Window::GetWidth() const
+{
+	
+	return m_width;
+}
+
+int Window::GetHeight() const
+{
+	return m_width;
+}
+
+int Window::GetWindowID() const
+{
+	return m_windowID;
+}
+
+GLFWwindow* Window::GetInternalWindow()
+{
+	return *m_internalWindow.get();
+}
+
+bool Window::HasMouseFocus() const
+{
+	return m_mouseFocus;
+}
+
+bool Window::HasKeyboardFocus() const
+{
+	return m_keyboardFocus;
+}
+
+bool Window::IsMinimized() const
+{
+	return m_minimized;
+}
+
+bool Window::IsShown() const
+{
+	return m_shown;
+}
+
---
+++ MayBeEngine/MayBeEngine/Display.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <GLFW/glfw3.h>
+#include <memory>
+
+class Window
+{
+public:
+	//Init internals
+	Window(int width, int height);
+
+
+	bool Initialize();
+	void Destroy();
+
+	//Get window properties
+	int GetWidth() const;
+	int GetHeight() const;
+
+
+	int GetWindowID() const;
+	GLFWwindow* GetInternalWindow();
+	bool HasMouseFocus() const;
+	bool HasKeyboardFocus() const;
+	bool IsMinimized() const;
+	bool IsShown() const;
+
+private:
+	//Display Kernel
+	std::unique_ptr<GLFWwindow*> m_internalWindow;
+	int m_windowID;
+
+	//Window properties
+	int m_width;
+	int m_height;
+
+	bool m_mouseFocus;
+	bool m_keyboardFocus;
+	bool m_fullScreen;
+	bool m_minimized;
+	bool m_shown;
+
+	
+};
